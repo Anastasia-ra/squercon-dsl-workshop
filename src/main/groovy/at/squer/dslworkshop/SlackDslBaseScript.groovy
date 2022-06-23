e4b5d481423e760dd472c62215dcc44c5ce214c5
Lukasz Juszczyk <lukasz@squer.at>
2022-06-23 13:01:15 +0200

    Add pause() to base script

--- src/main/groovy/at/squer/dslworkshop/SlackDslBaseScript.groovy
+++ src/main/groovy/at/squer/dslworkshop/SlackDslBaseScript.groovy
@@ -12,6 +12,10 @@ abstract class SlackDslBaseScript extends Script {
     abstract void scriptBody()
 
     // global script methods/functions must be declared here
+
+    def pause() {
+        System.in.read()
+    }
 }
 
 // classes must be declared here
